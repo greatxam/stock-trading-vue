327b1bb21848868cfc87f6674907a910590d3c96
Maximillian Estrada <maximillian_estrada@yahoo.com>
2024-07-05 12:33:31 +0800

    v0.0.5 - Authentication
    + Implement OAuth Login API
    + Environment variables
    + Store access token
    
    #5

--- src/stock-trading-vue/static/main.ts
+++ src/stock-trading-vue/static/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import StockTradingApp from './StockTradingApp.vue'
 import router from './router/'
+import axios from 'axios'
 
 import 'bootstrap-icons/font/bootstrap-icons.scss'
 import * as bootstrap from 'bootstrap'
 import './assets/styles.scss'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_PROXY
 
 const app = createApp(StockTradingApp)
 
